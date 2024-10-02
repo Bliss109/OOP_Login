522f3fe81f673ea0ee50148600592d84a25f998e
Bliss109 <cindyogutu0@gmail.com>
2024-10-02 22:27:37 +0300

    PDO connection for authentication database

--- form/register.php
+++ form/register.php
@@ -1,4 +1,28 @@
+<?php
+   DEFINE('HOST','localhost');
+   DEFINE('USERNAME', 'root');
+   DEFINE('PASSWORD', '');
+   DEFINE('DATABASE','authentication');
+
+   // Set up DSN (Data Source Name) for PDO with MySQL
+   $dsn = 'mysql:host='.HOST . ';dbname='.DATABASE;
+
+   try {
+       // Create a new PDO instance for database connection
+       $conn = new PDO($dsn, USERNAME, PASSWORD);
+       // Set the PDO error mode to exception
+       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+        $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); //for LIMITS
+   } catch (PDOException $e) {
+       // Handle the exception and print error message
+       echo 'Connection failed: ' . $e->getMessage();
+   }
+?>
+
+
 <?php include '../includes/header.php'?>
+
 <div class="container">
     <div class="row">
         <div class="col-md-5 mx-auto">
