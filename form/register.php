3d26c394da49e962ce9ca4433c92d09b29317ae8
Bliss109 <cindyogutu0@gmail.com>
2024-10-02 21:20:05 +0300

    registration form layout with fields

--- form/register.php
+++ form/register.php
@@ -3,12 +3,43 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
 </head>
+
 <body>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 mx-auto">
+                <form action="" method="POST">
+                    <div class="form-group">
+                     <label for="firstname">Firstname</label>
+                     <input type="text" name="firstname" id="firstname" clas="form-control">
+                    </div>
+                    <div class="form-group">
+                     <label for="lastname">Lastname</label>
+                     <input type="text" name="firstname" id="lastname" clas="form-control">
+                    </div>
+                    <div class="form-group">
+                     <label for="email">Email</label>
+                     <input type="email" name="email" id="email" clas="form-control">
+                    </div>
+                    <div class="form-group">
+                     <label for="password1">Password</label>
+                     <input type="password" name="password1" id="password1" clas="form-control">
+                    </div>
+                    <div class="form-group">
+                     <label for="password2">Confirm Password</label>
+                     <input type="password" name="password2" id="password2" clas="form-control">
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
