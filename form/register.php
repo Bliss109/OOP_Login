c78959c393ef592f62469978345a30eb5d6c5875
Bliss109 <cindyogutu0@gmail.com>
2024-10-02 21:35:18 +0300

    egistration form with header and footer includes,

--- form/register.php
+++ form/register.php
@@ -1,45 +1,35 @@
-<?php
+<?php include '../includes/header.php'?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-5 mx-auto">
+            <form action="" method="POST">
+                <h1>Register Here</h1>
+                <div class="form-group">
+                    <label for="firstname">Firstname</label>
+                    <input type="text" name="firstname" id="firstname" clas="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="lastname">Lastname</label>
+                    <input type="text" name="firstname" id="lastname" clas="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" clas="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="password1">Password</label>
+                    <input type="password" name="password1" id="password1" clas="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="password2">Confirm Password</label>
+                    <input type="password" name="password2" id="password2" clas="form-control">
+                </div>
+                <div class="d-grid">
+                    <button type="submit" class="btn btn-info" name="Register">Register</button>
+                </div>
+            </form>
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
-</head>
-
-<body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-5 mx-auto">
-                <form action="" method="POST">
-                    <div class="form-group">
-                     <label for="firstname">Firstname</label>
-                     <input type="text" name="firstname" id="firstname" clas="form-control">
-                    </div>
-                    <div class="form-group">
-                     <label for="lastname">Lastname</label>
-                     <input type="text" name="firstname" id="lastname" clas="form-control">
-                    </div>
-                    <div class="form-group">
-                     <label for="email">Email</label>
-                     <input type="email" name="email" id="email" clas="form-control">
-                    </div>
-                    <div class="form-group">
-                     <label for="password1">Password</label>
-                     <input type="password" name="password1" id="password1" clas="form-control">
-                    </div>
-                    <div class="form-group">
-                     <label for="password2">Confirm Password</label>
-                     <input type="password" name="password2" id="password2" clas="form-control">
-                    </div>
-                </form>
-
-            </div>
         </div>
     </div>
-</body>
-
-</html>
+</div>
+<?php include '../includes/footer.php' ?>
