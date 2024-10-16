c7575c83fec18f953f705e64e4adb26e042e8474
Bliss109 <cindyogutu0@gmail.com>
2024-10-16 12:49:20 +0300

    Index page

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
-include 'includes/header.php'
+    require_once 'config/config.php';
+    include('includes/header.php');
+
+
+    $user = '';
+    if(isset($_SESSION['user'])){
+        $user = $_SESSION['user'];
+        echo $user->firstname;
+    }else{
+        header('location: login.php');
+    }
+
+
 ?>
-  
-</body>
-</html>
+<div class="container">
+    <h1>Welcome <?php echo $user->firstname?>!</h1>
+</div>
+<?php include('includes/footer.php')?>
